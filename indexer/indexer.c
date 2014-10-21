163ce8ab26e9b515acc84f04b6167614e6c9983c
Mark Conley <MarkC@MarkC.MBP>
2014-10-21 11:04:54 -0400

    clean

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -13,22 +13,24 @@
 #include "indexer.h"
 #include "utlist.h"
 
-
 TokenPtr words = NULL;
 
 int isDirectoryEmpty( char *dirname ) {
   	int n = 0;
   	struct dirent *d;
 	DIR *dir = opendir(dirname);
+
  	if (dir == NULL){ //Not a directory or doesn't exist
    	return 1;
  	}
+
   	while ((d = readdir(dir)) != NULL) {
    	if(++n > 2){
       	break;
-    	}
+   	}
   	}
   	closedir(dir);
+
   	if (n <= 2){ //Directory Empty
    	return 1;
    } else {
