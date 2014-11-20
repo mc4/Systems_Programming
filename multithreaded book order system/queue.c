9d3b68f0ed4550908c8c4f83c03409375e6f8582
Mark Conley <MarkC@MarkC.MBP>
2014-11-20 14:06:16 -0500

    testing almost complete, should be thread safe

--- multithreaded book order system/queue.c
+++ multithreaded book order system/queue.c
@@ -4,25 +4,25 @@
 
 typedef struct QueueNode * QueueNodePtr;	
 struct QueueNode {
-	void *data;
+	void * data;
 	QueueNodePtr next;
 };
 
 /* First In First Out */
 typedef struct Queue Queue; 
 struct Queue {
-	int isOpen;			//flag for when the producer is done
-	QueueNodePtr head;	//dequeue here
-	QueueNodePtr tail;	//enqueue here
-	int length;
+	int isOpen;			// flag for when the producer is done
+	QueueNodePtr head;	// dequeue here
+	QueueNodePtr tail;	// enqueue here
+	int size;
 	pthread_mutex_t mutex;
 };
 
-Queue *Qcreate();
+Queue * Qcreate();
 void enqueue(Queue *, void *);
 void * dequeue(Queue *);
 void * Qpeek(Queue *);
-int Qlength(Queue *);
+int Qsize(Queue *);
 void Qdestroyer(Queue *);
 
 Queue * Qcreate(){
@@ -31,17 +31,17 @@ Queue * Qcreate(){
 	q->isOpen = 1;
 	q->head   = NULL;
 	q->tail   = NULL;
-	q->length = 0;
+	q->size = 0;
 	pthread_mutex_init(&q->mutex,0);
 	return q;
 }
 
-void enqueue(Queue *q, void *data){
+void enqueue(Queue * q, void * data){
 	pthread_mutex_lock(&q->mutex);
 	QueueNodePtr queueNode = malloc(sizeof(queueNode));
 	queueNode->data = data;
 	queueNode->next = NULL;
-	if(q->length == 0){
+	if(q->size == 0){
 		queueNode->next = NULL;
 		q->head = queueNode;
 		q->tail = queueNode;
@@ -49,21 +49,21 @@ void enqueue(Queue *q, void *data){
 		q->tail->next = queueNode;
 		q->tail = queueNode;
 	}
-	q->length++;
+	q->size++;
 	pthread_mutex_unlock(&q->mutex);
 }
 
-void * dequeue(Queue *q){
+void * dequeue(Queue * q){
 	pthread_mutex_lock(&q->mutex);
 
-	if(q->length == 0){
+	if(q->size == 0){
 		pthread_mutex_unlock(&q->mutex);
 		return NULL;
 	}
 
 	QueueNodePtr head = q->head;
 	void * data = head->data;
-	if(q->length == 1){		
+	if(q->size == 1){		
 		free(head);
 	} else {
 		q->head = q->head->next;
@@ -71,7 +71,7 @@ void * dequeue(Queue *q){
 
 	}
 
-	q->length--;
+	q->size--;
   	pthread_mutex_unlock(&q->mutex);
 	return data;
 }
@@ -82,13 +82,13 @@ void * Qpeek(Queue * q){
 	return NULL;
 }
 
-int Qlength(Queue *q){
-	return q->length;
+int Qsize(Queue * q){
+	return q->size;
 }
 
 /* dequeue frees nodes */
-void Qdestroyer(Queue *q){
-	while(q->length > 0){
+void Qdestroyer(Queue * q){
+	while(q->size > 0){
 		dequeue(q);
 	}
 	pthread_mutex_destroy(&q->mutex);
@@ -100,7 +100,7 @@ int main(void){
 	enqueue(q, (void*)-1);
 	enqueue(q, (void*)-2223);
 	enqueue(q, (void*)3333);
-	printf("q size is: %d = %d\n",q->length, Qlength(q));
+	printf("q size is: %d = %d\n",q->size, Qsize(q));
 
 	int i = (int)dequeue(q);
 	int j = (int)dequeue(q);
